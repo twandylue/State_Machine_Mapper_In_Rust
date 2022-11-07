9ec31443b8f54cb69ecab2337c028b0e9839a234
AndyLu <laserlue@gmail.com>
2022-11-07 13:49:16 +0800

    [refactor] in main.rs

--- state_machine_mapper/src/main.rs
+++ state_machine_mapper/src/main.rs
@@ -22,18 +22,12 @@ fn main() {
             println!();
 
             println!("* === Map in different languages ===");
-            let output_in_csharp = output_in_csharp(&s);
-            let output_in_js = output_in_js(&s);
-            let output_in_python = output_in_python(&s);
-            let output_in_cpp = output_in_cpp(&s);
-            let output_in_go = output_in_go(&s);
-            let output_in_rust = output_in_rust(&s);
-            println!("Map in rust: \n{output_in_rust}");
-            println!("Map in cpp: \n{output_in_cpp}");
-            println!("Map in go: \n{output_in_go}");
-            println!("Map in c_sharp: \n{output_in_csharp}");
-            println!("Map in js: \n{output_in_js}");
-            println!("Map in python: \n{output_in_python}");
+            println!("Map in rust: \n{}", output_in_rust(&s));
+            println!("Map in cpp: \n{}", output_in_cpp(&s));
+            println!("Map in go: \n{}", output_in_go(&s));
+            println!("Map in c_sharp: \n{}", output_in_csharp(&s));
+            println!("Map in js: \n{}", output_in_js(&s));
+            println!("Map in python: \n{}", output_in_python(&s));
         }
         Err(err) => println!("Application error: {}", err),
     }
